e924ffdfa8ebe4e329570f34df17b7538413f1b4
Rafal Kuzma <rafal.kuzma@it.abb.com>
2022-02-02 18:23:40 +0100

    fix

--- LoyaltyProgram/Transaction.cs
+++ LoyaltyProgram/Transaction.cs
@@ -11,7 +11,7 @@ namespace LoyaltyProgram
     {
         [Key]
         public int Id { get; set; }
-        public User? User { get; set; }
+        public int UserId { get; set; }
         public string? DeparturePlace { get; set; }
         public string? ArrivalePlace { get; set; }
         public string? FlightNumber { get; set; }
