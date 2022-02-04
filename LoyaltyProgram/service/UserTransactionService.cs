348087f75030862c3b6582253f3768507f0ac067
Jakub Cholewa <tom7177@gmail.com>
2022-02-04 22:46:08 +0100

    user window refactor

---
+++ LoyaltyProgram/service/UserTransactionService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoyaltyProgram.service
+{
+    public class UserTransactionService
+    {
+        private UserTransactionDataContext context;
+
+        public UserTransactionService()
+        {
+            context = new UserTransactionDataContext();
+        }
+
+        public void deleteTransaction(List<Transaction> userTransactions, Transaction transaction)
+        {
+            int transactionId = transaction.Id;
+            Transaction transactionToDelete = this.context.Transactions.Single(transaction => transaction.Id == transactionId);
+            context.Remove(transactionToDelete);
+            context.SaveChanges();
+        }
+
+        public List<Transaction> getUserTransactions(User user)
+        {
+            List<Transaction> userTransactions = new List<Transaction>();
+            bool transactionsFound = context.Transactions.Any();
+            
+            if (transactionsFound)
+            {
+                userTransactions = context.Transactions.Where(transaction => transaction.UserId == user.Id).ToList();
+            }
+            return userTransactions;
+        }
+
+        public double CountPoints(List<Transaction> userTransactions)
+        {
+            return (double)(userTransactions
+                .Where(transaction => transaction.IsVerified == true)
+                .Sum(transaction => transaction.Price) / 10);
+        }
+    }
+}
